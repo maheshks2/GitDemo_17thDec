90efff8f116fa98484e375c63127d5a02ef4380b
maheshks2 <ks.mahesh2@gmail.com>
2022-12-18 11:27:46 +0530

    new chages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a=10,b=20;
+        System.out.println(b-a);
     }
 }
